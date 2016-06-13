99fa849ad1e6481d1ecf7ee31903b9ba6572c305
Jason McCallister <themccallister@gmail.com>
2016-06-13 09:54:29 -0500

    PSR-2 and PHPDoc Blocks

--- fastly/FastlyPlugin.php
+++ fastly/FastlyPlugin.php
@@ -1,53 +1,89 @@
 <?php
+
 namespace Craft;
 
 class FastlyPlugin extends BasePlugin
 {
+    /**
+     * Initialize the plugin
+     */
+    public function init()
+    {
+        // Whenever an entry is saved, bust the Fastly cache
+        craft()->entries->onSaveEntry = function (Event $event) {
+            craft()->fastly->purgeFastlyCache();
+        };
+    }
 
-  public function init()
-  {
-    // Whenever an entry is saved, bust the Fastly cache
-    craft()->entries->onSaveEntry = function(Event $event) {
-      craft()->fastly->purgeFastlyCache();
-    };
-  }
+    /**
+     * Define plugins name
+     *
+     * @return mixed
+     */
+    public function getName()
+    {
+        return Craft::t('Fastly');
+    }
 
-  protected function defineSettings()
-  {
-      return array(
-        'fastlyApiKey' => array(
-            AttributeType::String, 'label' => 'Fastly API Key'
-        ),
-        'fastlyServiceId' => array(
-            AttributeType::String, 'label' => 'Fastly ServiceId'
-        ),
-      );
-  }
+    /**
+     * Define the version
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return '0.1';
+    }
 
-	function getName()
-	{
-		return Craft::t('Fastly');
-	}
 
-	function getVersion()
-	{
-		return '0.1';
-	}
+    /**
+     * The Developer
+     *
+     * @return string
+     */
+    public function getDeveloper()
+    {
+        return 'One Design Company';
+    }
 
-	function getDeveloper()
-	{
-		return 'One Design Company';
-	}
+    /**
+     * Developers website
+     *
+     * @return string
+     */
+    public function getDeveloperUrl()
+    {
+        return 'http://onedesigncompany.com';
+    }
 
-	function getDeveloperUrl()
-	{
-		return 'http://onedesigncompany.com';
-	}
+    /**
+     * Define the plugins settings
+     *
+     * @return array
+     */
+    protected function defineSettings()
+    {
+        return array(
+            'fastlyApiKey' => array(
+                'type' => AttributeType::String,
+                'label' => 'Fastly API Key'
+            ),
+            'fastlyServiceId' => array(
+                'type' => AttributeType::String,
+                'label' => 'Fastly ServiceId'
+            ),
+        );
+    }
 
-  public function getSettingsHtml()
-  {
-     return craft()->templates->render('fastly/_settings', array(
-         'settings' => $this->getSettings()
-     ));
-  }
+    /**
+     * Get the settings template
+     * 
+     * @return mixed
+     */
+    public function getSettingsHtml()
+    {
+        return craft()->templates->render('fastly/_settings', array(
+            'settings' => $this->getSettings()
+        ));
+    }
 }
